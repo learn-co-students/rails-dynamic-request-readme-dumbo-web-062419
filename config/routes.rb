df9275bb07bb97e1861c8d830db9e8624a88614f
github username <github email address>
2019-07-18 23:39:39 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,3 +3,5 @@ Rails.application.routes.draw do
   resources :posts, only: :show
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+# notes: tests passed, but the web page didn't work
+# would be good to come back to it later
